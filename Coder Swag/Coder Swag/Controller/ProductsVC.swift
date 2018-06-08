0e7d0ad3608f81967b8cce61b2d9abc36de812a7
Hassan Ashraf <Selv3R@Hassan-Ashrafs-MacBook-Pro.local>
2018-06-08 19:44:46 +0200

    All Products added to its collection views

---
+++ Coder Swag/Coder Swag/Controller/ProductsVC.swift
@@ -0,0 +1,43 @@
+//
+//  ProductVC.swift
+//  Coder Swag
+//
+//  Created by Hassan Ashraf on 6/8/18.
+//  Copyright © 2018 h2m. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var productsCollection: UICollectionView!
+    
+    private(set) public var products = [Product]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        productsCollection.delegate = self
+        productsCollection.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+
+    func initProducts(category: Category) {
+        products = DataService.instance.getProducts(forCategoryTitle: category.title)
+        navigationItem.title = category.title
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as? ProductCell {
+            let product = products[indexPath.row]
+            cell.updateViews(product: product)
+            return cell
+        }
+        return ProductCell()
+    }
+
+}
